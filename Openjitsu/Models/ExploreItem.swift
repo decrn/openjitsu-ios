cc0ff53bda397652990030511ac9640114240ce9
Denis Carnier <denis_carn@hotmail.com>
2019-01-13 23:51:27 +0100

    Update ExploreItem Model
    Use struct, add SwiftyJSON parser

--- Openjitsu/Models/ExploreItem.swift
+++ Openjitsu/Models/ExploreItem.swift
@@ -6,16 +6,34 @@
 //  Copyright © 2019 Denis Carnier. All rights reserved.
 //
 
-import UIKit
+import Foundation
+import SwiftyJSON
 
-class ExploreItem {
-    var title: String
-    var description: String
-    var photo: UIImage?
-    
-    init(title: String, description: String, photo: UIImage?) {
-        self.title = title
-        self.description = description
-        self.photo = photo
+// Using structs as per the Apple Developer guidelines
+// https://developer.apple.com/documentation/swift/choosing_between_structures_and_classes
+// Using SwiftyJSON: https://github.com/SwiftyJSON/SwiftyJSON#usage
+
+struct ExploreItem {
+    let title: String
+    let description: String
+    let image: UIImage
+}
+
+extension ExploreItem {
+    static func fromJSON(json: JSON) -> ExploreItem {
+        let title = json["name"].stringValue
+        let description = json["description"].stringValue
+        var image: UIImage
+        
+        // Some safety checks to make sure we're actually getting an image
+        do {
+            let imageData = try Data(contentsOf: URL(string: json["image"].stringValue)!)
+            image = UIImage(data: imageData, scale: 1.0)!
+        } catch {
+            // Load a local Asset if this failed
+            image = UIImage(named: "ExploreItem_Thumb")!
+        }
+        
+        return ExploreItem(title: title, description: description, image: image)
     }
 }
