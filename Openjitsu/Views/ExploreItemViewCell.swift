00f2d4605bcbf01c26813f6c0b5a50ae31d01e4e
Denis Carnier <denis_carn@hotmail.com>
2019-01-13 16:27:24 +0100

    Create LaunchScreen

---
+++ Openjitsu/Views/ExploreItemViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ExploreItemViewCell.swift
+//  Openjitsu
+//
+//  Created by Denis Carnier on 13/01/2019.
+//  Copyright © 2019 Denis Carnier. All rights reserved.
+//
+
+import UIKit
+
+class ExploreItemViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
